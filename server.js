264f195d962f085f28cf7673547c7285d092474c
mamer-amer <amer.rasool121@gmail.com>
2020-08-18 00:53:28 -0700

    add first GET route

--- server.js
+++ server.js
@@ -3,6 +3,28 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 8080
 app.set('port',port);
+
+const mockUserData=[
+    {name:'Mark'},
+    {name:'Jill'}
+    ]
+
+
+app.get('/',(req,res)=>{
+    res.json({
+        success:true,
+        message:"Successsfully rendered",
+        users:mockUserData
+    })
+})
+
+
+
+
+
+
+
+
 app.listen(port,()=>{
     console.log("Server is listening on port number ",app.get('port'))
 })
